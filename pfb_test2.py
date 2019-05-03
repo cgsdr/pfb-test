ff53ec4ae92730d8c246abdc9ca1a432e852e236
Chris Shaw <chris@cgsdr.com>
2019-05-03 09:29:38 -0400

    Tweak pfb_test2.py to remove rotator and add comments.

--- pfb_test2.py
+++ pfb_test2.py
@@ -26,12 +26,12 @@ class pfb_test2(gr.top_block):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 4.8e6
+        self.samp_rate = samp_rate = 12.8e6
 
         self.variable_low_pass_filter_taps_0 = variable_low_pass_filter_taps_0 = firdes.low_pass(1.0, samp_rate, 20000, 5000, firdes.WIN_HAMMING, 6.76)
 
         self.Noversample = Noversample = 1
-        self.M = M = 192
+        self.M = M = 512
 
         ##################################################
         # Blocks
@@ -44,9 +44,9 @@ class pfb_test2(gr.top_block):
         self.pfb_channelizer_ccf_0.set_channel_map(([]))
         self.pfb_channelizer_ccf_0.declare_sample_delay(0)
 
-        self.osmosdr_source_0 = osmosdr.source( args="numchan=" + str(1) + " " + 'soapy=0,driver=remote,remote=tcp://cgsdr-R1:55132' )
+        self.osmosdr_source_0 = osmosdr.source( args="numchan=" + str(1) + " " + 'soapy=0,driver=remote,remote=tcp://localhost:55132' )
         self.osmosdr_source_0.set_sample_rate(samp_rate)
-        self.osmosdr_source_0.set_center_freq(470887500, 0)
+        self.osmosdr_source_0.set_center_freq(470850000, 0)
         self.osmosdr_source_0.set_freq_corr(0, 0)
         self.osmosdr_source_0.set_dc_offset_mode(0, 0)
         self.osmosdr_source_0.set_iq_balance_mode(0, 0)
@@ -57,15 +57,16 @@ class pfb_test2(gr.top_block):
         self.osmosdr_source_0.set_antenna('', 0)
         self.osmosdr_source_0.set_bandwidth(0, 0)
 
-        self.blocks_udp_sink_0 = blocks.udp_sink(gr.sizeof_gr_complex*1, 'localhost', 7355, 1472, True)
-        self.blocks_rotator_cc_0 = blocks.rotator_cc(-3.14159*0.0375/2.4)
+        self.blocks_udp_sink_0 = blocks.udp_sink(gr.sizeof_gr_complex*1, 'localhost', 7355, 1472, True) # Change 'localhost' to IP address of UDP sink
+        # self.blocks_rotator_cc_0 = blocks.rotator_cc(-3.14159*0.0375/2.4) # This rotator shifts the center of the band to a 25 kHz boundary so all the pfb channels line up.
         self.blocks_null_sink_0 = blocks.null_sink(gr.sizeof_gr_complex*1)
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blocks_rotator_cc_0, 0), (self.pfb_channelizer_ccf_0, 0))
-        self.connect((self.osmosdr_source_0, 0), (self.blocks_rotator_cc_0, 0))
+        # self.connect((self.blocks_rotator_cc_0, 0), (self.pfb_channelizer_ccf_0, 0))
+        # self.connect((self.osmosdr_source_0, 0), (self.blocks_rotator_cc_0, 0))
+        self.connect((self.osmosdr_source_0, 0), (self.pfb_channelizer_ccf_0, 0))
         for i in range(M-1):
             self.connect((self.pfb_channelizer_ccf_0, i+1), (self.blocks_null_sink_0, i))
         self.connect((self.pfb_channelizer_ccf_0, 0), (self.blocks_udp_sink_0, 0))
